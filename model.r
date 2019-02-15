fa750aa6c68bff0303f8ab1d6ce7f1a86dd0cb28
DenisValeev <denis.valeev@gmail.com>
2019-02-15 21:07:31 +0300

    Update model.r

--- model.r
+++ model.r
@@ -1,15 +1,15 @@
-# Читаем данные
+# Parse the data
 library(readr)
 Input <- read_delim("input.csv", "\t", escape_double = FALSE, locale = locale(decimal_mark = "."), na = "NULL", trim_ws = TRUE)
 head(Input)
 #View(Input)
 
-# Обучаем и валидируем
+# Train and validate
 require(xgboost)
 s <- sample.int(.5*nrow(Input), replace = F)
-# Указываем целевой столбец
+# Set the target column
 target <- Input$y
-# Удаляем целевые столбцы и столбцы с высоким значением Gain (importance)
+# Remove the target and, if necessary, additional columns
 drops <- tolower(c("y","applicationid"))
 Input <- Input[, !(tolower(colnames(Input)) %in% drops), drop = F]
 dtrain <- xgb.DMatrix(data = as.matrix(sapply(Input[s,], as.numeric)), label=target[s])
@@ -17,13 +17,13 @@ dtest <- xgb.DMatrix(data = as.matrix(sapply(Input[-s,], as.numeric)), label=tar
 watchlist <- list(train=dtrain, test=dtest)
 bst <- xgb.train(data = dtrain, max_depth = 1, eta = 1, subsample = 1, colsample_bytree = 1, nthread = 16, nrounds = 60, objective = "binary:logitraw", missing = NA, watchlist=watchlist)
 
-# Дампим
+# Dump
 xgb.dump(bst, "model.xgb")
 
-# Важность фичей
+# Feature importance
 sink("importance.txt")
 xgb.importance(colnames(Input), model = bst) 
 sink()
 
-# Столбцы в отдельный файл
+# Column names
 write.table(colnames(Input), file = "features.csv", append = F, quote = F, eol = "\t", row.names = F, col.names = T)
